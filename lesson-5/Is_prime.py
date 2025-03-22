c6c490ffe9a9b36f6f57b88396d527d412a46622
gulhayoxolmurotova <g.xolmurotova@newuu.uz>
2025-03-22 13:27:26 +0500

    updated

---
+++ lesson-5/Is_prime.py
@@ -0,0 +1,16 @@
+import math
+
+def isPrime():
+    number = int(input("Enter a number: "))
+    
+    if number < 2:
+        print("It is not a prime number")
+        return
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            print(False)
+            return
+    
+    print(True)
+
+isPrime()

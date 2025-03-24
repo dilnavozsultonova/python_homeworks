ee3f4a8412fe9bc6459f0631aec73196406ec188
Dilnavoz Sultonova <dilnavozsultonova@Air-Akbar.uwed.uz>
2025-03-24 22:22:19 +0500

    not ready updated

--- lesson-6/homework/file_handling.py
+++ lesson-6/homework/file_handling.py
@@ -1,2 +1,13 @@
-file=open("employee.txt")
-file.write("banana")
+def add_employee():
+    with open("employee.txt","a") as file:
+        print("Enter the employee details")
+
+        employee_id=input("Employee ID: ")
+        name= input("Name: ")
+        position = input("Position: ")
+        salary=input("Salary: ")
+
+        record=f"{employee_id},{name},{salary}\n"
+
+        file.write(record)
+        print("Employee record added successfully")

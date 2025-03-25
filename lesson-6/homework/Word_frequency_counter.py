fe402e2eb9054852991526f9bbf27d9bf1fba3ec
Dilnavoz Sultonova <d.sultonova@172-10-60-156.lightspeed.moblal.sbcglobal.net>
2025-03-25 17:10:11 +0500

    not ready

---
+++ lesson-6/homework/Word_frequency_counter.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+cur_dir = Path(__file__).resolve().parent
+
+try:
+    with open(cur_dir / "sample.txt","r"):
+        pass
+except:
+    with open(cur_dir / "sample.txt","w") as file:
+        etc=input("Enter your paragraph: ")
+        file.write(etc)
+file=open(cur_dir / "sample.txt","r")
+
+cnt = dict()
+
+s = ""
+for line in file.readlines():
+    for i in line:
+        if i.isalpha():
+            s += i.lower()
+        else:
+            if s!="":
+                if s in cnt.keys():
+                    cnt[s] += 1
+                else:
+                    cnt[s] = 1
+            s = ""
+
+    if s!="":
+        if s in cnt.keys():
+            cnt[s] += 1
+        else:
+            cnt[s] = 1
+
+l = []
+total = 0
+
+for key, value in cnt.items():
+    l.append([value,key])
+    total += value
+
+l.sort(reverse=True)
+
+
+
+file=open(cur_dir / "word_count_report.txt","w")
+file.write(f"Word Count Report\nTotal words: {total}\n")
+for i in range(min(len(l),5)):
+    file.write(f"{l[i][1]} - {l[i][0]}\n")
